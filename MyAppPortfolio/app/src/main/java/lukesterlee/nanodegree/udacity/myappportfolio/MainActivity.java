5ffe26d4f4477c4f24e73644e55ba896892f06c5
Luke Lee <samsung3star@gmail.com>
2015-06-02 16:31:41 -0400

    added buttons

--- MyAppPortfolio/app/src/main/java/lukesterlee/nanodegree/udacity/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/lukesterlee/nanodegree/udacity/myappportfolio/MainActivity.java
@@ -4,14 +4,58 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button buttonSpotify;
+    Button buttonScores;
+    Button buttonLibrary;
+    Button buttonBuild;
+    Button buttonReader;
+    Button buttonCapstone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSpotify = (Button) findViewById(R.id.button_spotify);
+
+        buttonScores = (Button) findViewById(R.id.button_scores);
+
+        buttonLibrary = (Button) findViewById(R.id.button_library);
+
+        buttonBuild = (Button) findViewById(R.id.button_build);
+
+        buttonReader = (Button) findViewById(R.id.button_reader);
+
+        buttonCapstone = (Button) findViewById(R.id.button_capstone);
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.button_spotify:
+                    break;
+                case R.id.button_scores:
+                    break;
+                case R.id.button_library:
+                    break;
+                case R.id.button_build:
+                    break;
+                case R.id.button_reader:
+                    break;
+                case R.id.button_capstone:
+                    break;
+            }
+
+        }
     }
 
 
