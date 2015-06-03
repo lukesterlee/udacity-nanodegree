e5426819308fc552ac8e110dba982d1a7e313441
Luke Desktop <lukesterlee@gmail.com>
2015-06-03 14:25:08 -0400

    applied feedback from Udacity

--- MyAppPortfolio/app/src/main/java/lukesterlee/nanodegree/udacity/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/lukesterlee/nanodegree/udacity/myappportfolio/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-
 public class MainActivity extends ActionBarActivity {
 
     Button buttonSpotify;
@@ -47,40 +46,22 @@ public class MainActivity extends ActionBarActivity {
             button.setOnClickListener(listener);
         }
 
+    }
 
+    public void displayToast(View view) {
+        Button button = (Button) view;
+        String appName = button.getText().toString();
+        String toastMessage = getString(R.string.open_app) + appName;
+        int toastLength = Toast.LENGTH_SHORT;
+        Toast.makeText(getApplicationContext(), toastMessage, toastLength).show();
     }
 
     private class ButtonClickListener implements View.OnClickListener {
-
-
-
         @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.button_spotify:
-                    Toast.makeText(getApplicationContext(), "This button will launch my Spotify Streamer App!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.button_scores:
-                    Toast.makeText(getApplicationContext(), "This button will launch my Football Scores App!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.button_library:
-                    Toast.makeText(getApplicationContext(), "This button will launch my Library App!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.button_build:
-                    Toast.makeText(getApplicationContext(), "This button will launch my Build It Bigger App!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.button_reader:
-                    Toast.makeText(getApplicationContext(), "This button will launch my XYZ Reader App!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.button_capstone:
-                    Toast.makeText(getApplicationContext(), "This button will launch my Capstone App!", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-
-
+        public void onClick(View button) {
+            displayToast(button);
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -100,7 +81,6 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
