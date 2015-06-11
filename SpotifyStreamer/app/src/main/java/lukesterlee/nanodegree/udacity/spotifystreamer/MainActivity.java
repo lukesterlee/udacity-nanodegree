a314e06b55338aa1f4672bd39e0c82f16b1a1244
Luke Desktop <lukesterlee@gmail.com>
2015-06-11 17:49:43 -0400

    saved

--- SpotifyStreamer/app/src/main/java/lukesterlee/nanodegree/udacity/spotifystreamer/MainActivity.java
+++ SpotifyStreamer/app/src/main/java/lukesterlee/nanodegree/udacity/spotifystreamer/MainActivity.java
@@ -1,15 +1,23 @@
 package lukesterlee.nanodegree.udacity.spotifystreamer;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,19 +35,30 @@ public class MainActivity extends ActionBarActivity {
 //    private static final String CLIENT_ID = "39e7fc61588a407bb2fb2f92e045ccb1";
 //    private static final String CLIENT_SECRET_ID = "5d27fd83b77e4438b332119203a8f89b";
 
-    private static final String ENDPOINT = "https://api.spotify.com/v1/artists/";
-    private static final String TOP_TRACKS = "/top-tracks";
+    EditText searchArtistEditText;
+    ListView searchResultListView;
 
-    String bey = "https://api.spotify.com/v1/search?q=Beyonce&type=artist";
 
-    String key = "https://api.spotify.com/v1/artists/6vWDO969PvNqNYHIOW5v0m/top-tracks?country=US";
+
+    // https://api.spotify.com/v1/search?q=Beyonce&type=artist
+    private static final String ENDPOINT_SEARCH = "https://api.spotify.com/v1/search?q=";
+    private static final String SEARCH_SUFFIX = "&type=artist";
+
+    // https://api.spotify.com/v1/artists/6vWDO969PvNqNYHIOW5v0m/top-tracks?country=US
+    private static final String ENDPOINT_TOP_TRACKS = "https://api.spotify.com/v1/artists/";
+    private static final String TOP_TRACKS_SUFFIX = "/top-tracks?country=US";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SpotifyApi api = new SpotifyApi();
+        initializeViews();
+
+
 
         Button button = (Button) findViewById(R.id.button_top);
         button.setOnClickListener(new View.OnClickListener() {
@@ -51,10 +70,17 @@ public class MainActivity extends ActionBarActivity {
         });
 
 
-        //Picasso.with(getApplicationContext()).load("htt").into();
+    }
 
+    private void initializeViews() {
+        searchArtistEditText = (EditText) findViewById(R.id.editText_search_artist);
+        searchResultListView = (ListView) findViewById(R.id.listView_search_result);
+        searchResultListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
+            }
+        });
     }
 
 
@@ -78,5 +104,28 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AsyncLoading extends AsyncTask<Void, Void, List<TopTrackItem>> {
+
+        @Override
+        protected List<TopTrackItem> doInBackground(Void... params) {
+            try {
+                return new TopTracksGetter().getTopTracksList();
+            } catch (JSONException e) {
+                Log.e("JSON", "couldn't get Json image.");
+            } catch (IOException e) {
+                Log.e("JSON", "couldn't get Json string.");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<TopTrackItem> topTrackList) {
+            if (topTrackList != null) {
+                adapter = new TopTrackAdapter(getApplicationContext(), R.layout.list_item_top_tracks, topTrackList);
+                mListView.setAdapter(adapter);
+            }
+        }
     }
 }
---
+++ SpotifyStreamer/app/src/main/java/lukesterlee/nanodegree/udacity/spotifystreamer/SearchResultItem.java
@@ -0,0 +1,9 @@
+package lukesterlee.nanodegree.udacity.spotifystreamer;
+
+/**
+ * Created by Luke on 6/11/2015.
+ */
+public class SearchResultItem {
+    private
+    private String thumbnailUrl;
+}
